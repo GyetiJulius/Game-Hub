54300c7aa450a49033b555867e62acdb64ef13a8
Julius Gyeti 10989434 <jgyeti@st.ug.edu.gh>
2024-05-03 01:21:03 -1200

    Added the game card component

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "06d7e27052234204b867e8dda255f902",
+    key: "906f36e4c0ff46da8bf7db7640725985",
   },
 });
