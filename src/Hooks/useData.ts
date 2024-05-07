396d26568004f1bbb42c5d3f1d5102002584b461
Julius Gyeti 10989434 <jgyeti@st.ug.edu.gh>
2024-05-07 04:01:16 -1200

    Created a generic hook for all use cases

---
+++ src/Hooks/useData.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+const useGenres= <T>(endpoint: string) => {
+  const [data, setGenre] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLaoding, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setLoading(false);
+        setGenre(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return { data, error, isLaoding };
+};
+
+export default useGenres;
